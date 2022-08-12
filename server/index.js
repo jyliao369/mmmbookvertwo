8f1a83703dfea1a8298dd263c3f597fc1fafe060
jyliao369 <jyliao369@gmail.com>
2022-08-12 16:34:42 -0400

    initial commit

--- server/index.js
+++ server/index.js
@@ -1,6 +1,19 @@
 const express = require("express");
+const mysql = require("mysql2");
 
 const app = express();
+
+// MYSQL CONNECTION
+// const db = mysql.createPool({
+//   host:,
+//   user:,
+//   password:,
+//   useranme:,
+// })
+
+app.use("/", (req, res) => {
+  res.json({ message: "call on me valerie, come and see me" });
+});
 
 // CHECKS FOR CONNECTION TO SERVER
 const PORT = process.env.PORT || 3001;
